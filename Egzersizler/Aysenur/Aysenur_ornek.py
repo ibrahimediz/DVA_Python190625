008624000523f05678f1da57a79ebbdfa847d562
İbrahim EDİZ <edizdesign@gmail.com>
2025-06-19 14:11:48 +0000

    19062025_3

--- Egzersizler/Aysenur/Aysenur_ornek.py
+++ Egzersizler/Aysenur/Aysenur_ornek.py
@@ -4,4 +4,18 @@
 
 # iki açısı kullanıcı tarafından girilen bir üçgeninn tipini ekrana yazdıran python kodunu yazınız
 # 45 - 45 ==> ikizkenar dik üçgen
+a = int(input("1. açı: "))
+b = int(input("2. açı: "))
 
+
+# Geçerli üçgen mi kontrolü
+if  a <= 0 or b <= 0:
+    print("Geçersiz açı pozitif olmalı.")
+else:
+    # Üçgen tipi belirleme
+    if a == b and a == 45:
+        print("İkizkenar dik üçgen")
+    elif a == b or a == 60:
+        print("Eşkenar üçgen")
+    else:
+        print("Belirsiz")
--- Egzersizler/Cevaplar/Cevaplar_ornek.py
+++ Egzersizler/Cevaplar/Cevaplar_ornek.py
@@ -2,3 +2,26 @@
 # iki açısı kullanıcı tarafından girilen bir üçgeninn tipini ekrana yazdıran python kodunu yazınız
 # 45 - 45 ==> ikizkenar dik üçgen
 
+aci1 = input("1. Açıyı Giriniz:")
+aci2 = input("2. Açıyı Giriniz:")
+if aci1 and aci2:
+    if aci1.isdigit() and aci2.isdigit():
+        aci1,aci2 = int(aci1),int(aci2)
+        l1 = [aci1,aci2,180-(aci1+aci2)]
+        if sum(l1) == 180:
+            k1 = set(l1)
+            if len(k1) == 2:
+                print("İkizkenar Üçgen")
+            elif len(k1) == 1:
+                print("Eşkenar Üçgen")
+            else:
+                print("Çeşit kenar üçgen")
+            if 90 in k1:
+                print("Dik Üçgen")
+        else:
+            print("Açılar Hatalı")
+    else:
+        print("Açılar Sayı olmalıdır")
+else:
+    print("Giriş Yapılmadı")
+
