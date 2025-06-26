4b3c3fefd11b4c4dd5d1203274b8a12154b802d2
İbrahim EDİZ <edizdesign@gmail.com>
2025-06-26 16:08:04 +0300

    26062025_1

---
+++ Egzersizler/MehmetAli/MehmetAli_dosyaornek.py
@@ -0,0 +1,8 @@
+
+l1 = ["ankara","istanbul","izmir","antalya","bursa"]
+l2 = ["ali","ayşe","fatma","veli","şermin"]
+l3 = [200,400,611,855,1100]
+from random import choice
+
+# yukarıda yer alan kod satırlarından faydalanarak dosyaornek.csv dosyasına en az 30 satırlık bir veri kaydediniz
+
